d158271be1125b0e4af2749bbf93e0196e600dad
Hoai <vuxuanhoai28@gmail.com>
2022-09-06 09:18:29 +0700

    add test.java file

--- test.java
+++ test.java
@@ -14,17 +14,7 @@ public class test
         int t=scan.nextInt();
         while(t-->0)
         {
-            int n = scan.nextInt();
-            ArrayList <Integer> arr = new ArrayList<Integer>(n);
-            for(int i=0;i<=n;i++)
-            {
-                arr.add(i);
-            }
-            // System.out.println(arr);
-            for(int i=0;i<arr.size();i++)
-            {
-                System.out.print(arr.get(i) + " ");
-            }
+            
         }
     }
 }
